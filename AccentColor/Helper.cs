a7885ca1c8454d68ad08cc05be16f4567b658663
Pieter van Zutven <pietervanzutven@outlook.com>
2021-12-29 20:37:15 +0100

    Renew color json url

--- AccentColor/Helper.cs
+++ AccentColor/Helper.cs
@@ -109,7 +109,7 @@ namespace AccentColor
             try
             {
                 HttpClient httpClient = new HttpClient();
-                HttpResponseMessage response = await httpClient.GetAsync(new Uri("https://jsonstorage.net/api/items/59119bb7-7ed4-40de-a6b4-cca0c3b67e39"));
+                HttpResponseMessage response = await httpClient.GetAsync(new Uri("https://api.jsonstorage.net/v1/json/076d7eeb-61fe-4a6e-b0cb-23094644edda"));
                 string responsetext = await response.Content.ReadAsStringAsync();
                 string[] colortext = JsonObject.Parse(responsetext).GetNamedString("color").Split(',');
                 return Color.FromArgb(0xff, Convert.ToByte(colortext[0]), Convert.ToByte(colortext[1]), Convert.ToByte(colortext[2]));
